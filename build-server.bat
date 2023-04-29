b711d35057c59f9143531014615e03d9a863dd9e
Anuwat <admin@unknownclub.net>
2023-04-29 23:55:27 +0700

    feat: update build command

--- build-server.bat
+++ build-server.bat
@@ -1,11 +1,11 @@
 @echo off
 
 xcopy /s /e .\assets\* .\dist-projects\dist-default\assets\
-xcopy /s /e .\prisma\schema.prisma .\dist-projects\dist-default\
+xcopy /s /e .\prisma\db.mongodb.schema.prisma .\dist-projects\dist-default\
 xcopy /s /e .\assets\scripts\start-pattaya-server.bat .\dist-projects\dist-default\
 cd .\dist-projects\dist-default\
 npm i
-npx prisma generate --schema=./schema.prisma
+npx prisma generate --schema=./db.mongodb.schema.prisma
 npx prisma migrate dev --name init
 
 call .\build-server-start.bat
