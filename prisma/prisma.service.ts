5601f72bb1208ad13adad593eba72444b9d84ba8
Anuwat <admin@unknownclub.net>
2023-04-30 10:08:26 +0700

    feat: fix bug

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -5,10 +5,57 @@ import { PrismaClient } from '@prisma/client';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+
+    process.on('SIGINT', async () => {
+      await this.bot.deleteMany()
+      await this.task.deleteMany()
+      await this.$disconnect();
+      process.exit(0);
+    });
+
+    process.on('SIGTERM', async () => {
+      await this.bot.deleteMany()
+      await this.task.deleteMany()
+      await this.$disconnect();
+      process.exit(0);
+    });
+
+    process.on('SIGKILL', async () => {
+      await this.bot.deleteMany()
+      await this.task.deleteMany()
+      await this.$disconnect();
+      process.exit(0);
+    });
+
+    process.on('SIGHUP', async () => {
+      await this.bot.deleteMany()
+      await this.task.deleteMany()
+      await this.$disconnect();
+      process.exit(0);
+    });
+
+    process.on('SIGUSR1', async () => {
+      await this.bot.deleteMany()
+      await this.task.deleteMany()
+      await this.$disconnect();
+      process.exit(0);
+    });
+
+
+    process.on('SIGUSR2', async () => {
+      await this.bot.deleteMany()
+      await this.task.deleteMany()
+      await this.$disconnect();
+      process.exit(0);
+    });
+
   }
 
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
+      await this.bot.deleteMany()
+      await this.task.deleteMany()
+      await this.$disconnect();
       await app.close();
     });
   }
